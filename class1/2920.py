69c419b502ffae8a8d503531c0d88acf90a66edb
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-04-30 21:43:27 +0900

    Feat: [2920] 음계

---
+++ class1/2920.py
@@ -0,0 +1,8 @@
+n_list = list(map(int, input().split()))
+
+if n_list == sorted(n_list):
+    print('ascending')
+elif n_list == sorted(n_list, reverse=True):
+    print('descending')
+else:
+    print('mixed')
